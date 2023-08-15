2a33f2f8f1dbc00f84ffd54ab87011e08dbc20c0
Sahan Bandara <92173358+sahanbandara23623@users.noreply.github.com>
2023-08-15 10:43:23 +0530

    some changes
    some changes

--- frontend/src/components/Carousel/index.tsx
+++ frontend/src/components/Carousel/index.tsx
@@ -13,6 +13,7 @@ import image1 from "../../assets/carouselImages/1.jpg";
 import image2 from "../../assets/carouselImages/2.jpg";
 import image3 from "../../assets/carouselImages/3.jpg";
 
+
 const images = [image1, image2, image3];
 
 const useStyles = createStyles((theme) => ({
