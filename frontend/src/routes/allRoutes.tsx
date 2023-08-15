f79edff0eda97704515f2d2ca77e64dfe4590f08
Venura Kaushan <86141130+VenuraKaushan@users.noreply.github.com>
2023-08-15 10:32:02 +0530

    Fix some errors in frontend

--- frontend/src/routes/allRoutes.tsx
+++ frontend/src/routes/allRoutes.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import LandingPage from "../pages/landing";
 import { Register } from "../components/Register";
 import ManageMembersPage from "../pages/ManageMembers";
 const AllRoutes = () => {
+  const client = new QueryClient();//config query client
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path ="/Register" element={<Register/>}/>
-        <Route path = "/admin/manageMember" element={<ManageMembersPage/>}/>
-      </Routes>
-    </Router>
+    <QueryClientProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/admin/manageMember" element={<ManageMembersPage />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
