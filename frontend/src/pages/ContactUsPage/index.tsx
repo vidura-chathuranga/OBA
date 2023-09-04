c4653e5eae0cad6c505b27b0f7727646912688ce
Vinnath Pathirana <125301413+VinnathPathirana@users.noreply.github.com>
2023-09-04 21:25:38 +0530

    add Contact Us page

---
+++ frontend/src/pages/ContactUsPage/index.tsx
@@ -0,0 +1,22 @@
+
+import { ContactUs } from "../../components/contactUs";
+import Footer from "../../components/footer/footer";
+import DefaultHeader from "../../components/defaultHeader";
+
+
+const ContactUsPage= ()=>{
+
+
+    return(
+        <div>
+           
+           <DefaultHeader/>
+           <ContactUs/>
+            <Footer/>
+            
+
+        </div>
+    )
+}
+
+export default ContactUsPage;
